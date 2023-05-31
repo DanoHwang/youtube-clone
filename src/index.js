8bc58312c799ebb8c6cf7d073881371c3de90a67
DanoHwang <danohwang00@gmail.com>
2023-05-31 14:57:25 +0900

    Feat: 라우터 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, element: <Videos />}, // 인덱스가 최상위인 경우
+      {path: 'videos', element: <Videos />}, // 사용자가 명시적으로 videos라고 한 경우
+      {path: 'videos/:keyword', element: <Videos />}, // videos 안에 키워드 param이 들어온 경우
+      {path: 'videos/watch/:videoId', element: <VideoDetail />}
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
