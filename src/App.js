7b810dbe9a383e8053026a8270bef300d54976cd
DanoHwang <danohwang00@gmail.com>
2023-06-04 20:40:03 +0900

    Feat: API 요청마다 인스턴스가 생성되지 않도록 context API 활용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { YoutubeApiProvider } from "./context/YoutubeApiContext";
 
 import Header from "./components/Header";
 
@@ -10,10 +11,12 @@ function App() {
   return (
     <>
       <Header />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+          <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube from '../api/youtube';
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const { isLoading, error, data: videos } = useQuery(
     ['videos', keyword],
-    () => {
-      const youtube = new FakeYoutube();
-      // const youtube = new Youtube();
-      return youtube.search(keyword);
-    },
+    () =>  youtube.search(keyword),
     { staleTime: 1000 * 60 * 5 }
   );
 
